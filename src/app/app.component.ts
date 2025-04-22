05a000ef4c6f6f1904eaa80902d89d4a64bdb646
carlos-henreis <carlos_henreis@yahoo.com.br>
2025-04-21 22:10:59 -0300

    create examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { IconTestComponent } from './components/icon-test.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    IconTestComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
